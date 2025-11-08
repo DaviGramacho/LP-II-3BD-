e545d3b08d0df43b8778a906ddc75e81434241f2
Davi Gramacho <piresdavi135@gmail.com>
2025-11-08 17:46:55 -0300

    :sparkles: feat: adicionando funcionalidade para a classe 'MsgAudio'

--- programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/MsgAudio.java
+++ programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/MsgAudio.java
@@ -1,4 +1,17 @@
-package main.java.edu.fatec.lp2.exercicio1;
+package edu.fatec.lp2.exercicio1.model;
 
 public class MsgAudio extends Mensagem {
+    private int duracao; // duração em segundos
 
+    public MsgAudio(Contatinho destinatario, String horaEnvio, String conteudo, int duracao) {
+        super(destinatario, horaEnvio, conteudo);
+        this.duracao = duracao;
+    }
+
+    public int getDuracao() { return duracao; }
+
+    @Override
+    public Mensagem sendMessage(String novoConteudo) {
+        return new MsgAudio(getDestinatario(), getHoraEnvio(), novoConteudo, duracao);
+    }
+}
