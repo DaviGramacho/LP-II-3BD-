8c9f53730c2e181a103b9673e5af543623be3ade
Davi Gramacho <piresdavi135@gmail.com>
2025-11-08 17:45:58 -0300

    :sparkles: feat: adicionando funcionalidade para a classe 'texto'

--- programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/MsgTexto.java
+++ programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/MsgTexto.java
@@ -1,5 +1,17 @@
-package main.java.edu.fatec.lp2.exercicio1;
+package edu.fatec.lp2.exercicio1.model;
 
 public class MsgTexto extends Mensagem {
+    private int numChar;
 
+    public MsgTexto(Contatinho destinatario, String horaEnvio, String conteudo) {
+        super(destinatario, horaEnvio, conteudo);
+        this.numChar = conteudo.length();
+    }
+
+    public int getNumChar() { return numChar; }
+
+    @Override
+    public Mensagem sendMessage(String novoConteudo) {
+        return new MsgTexto(getDestinatario(), getHoraEnvio(), novoConteudo);
+    }
 }
