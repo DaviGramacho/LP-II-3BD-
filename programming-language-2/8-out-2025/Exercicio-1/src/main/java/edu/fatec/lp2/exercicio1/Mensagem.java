aa16b92f01fa00a78e61f7a164e5487e6365305a
Davi Gramacho <piresdavi135@gmail.com>
2025-11-08 17:43:43 -0300

    :sparkles: feat: adicionando funcionalidade para a classe 'mensagem'

--- programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/Mensagem.java
+++ programming-language-2/8-out-2025/Exercicio-1/src/main/java/edu/fatec/lp2/exercicio1/Mensagem.java
@@ -1,7 +1,24 @@
-package main.java.edu.fatec.lp2.exercicio1;
-
-import main.java.edu.fatec.lp2.exercicio1.Contatinho;
+package edu.fatec.lp2.exercicio1.model;
 
 public abstract class Mensagem {
+    private Contatinho destinatario;
+    private String horaEnvio;
+    private String conteudo;
 
+    public Mensagem(Contatinho destinatario, String horaEnvio, String conteudo) {
+        this.destinatario = destinatario;
+        this.horaEnvio = horaEnvio;
+        this.conteudo = conteudo;
+    }
+
+    public Contatinho getDestinatario() { return destinatario; }
+    public String getHoraEnvio() { return horaEnvio; }
+    public String getConteudo() { return conteudo; }
+
+    public abstract Mensagem sendMessage(String novoConteudo);
+
+    @Override
+    public String toString() {
+        return horaEnvio + " - " + destinatario.getNome() + ": " + conteudo;
+    }
 }
